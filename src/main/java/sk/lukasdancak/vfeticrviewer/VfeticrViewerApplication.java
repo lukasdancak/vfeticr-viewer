89c18e2167ffb4868a3ebd33415a25f31f5abfa8
Lukas <lukas.dancak@gmail.com>
2026-06-09 01:47:13 +0200

    Initial commit: Spring Boot project vfeticr-viewer

---
+++ src/main/java/sk/lukasdancak/vfeticrviewer/VfeticrViewerApplication.java
@@ -0,0 +1,13 @@
+package sk.lukasdancak.vfeticrviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfeticrViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VfeticrViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/lukasdancak/vfeticrviewer/VfeticrViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.lukasdancak.vfeticrviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfeticrViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
